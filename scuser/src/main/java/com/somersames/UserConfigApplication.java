a73a0b311acf7c0bdb92d83de6c03f12dafca8f0
somersames@gmail.com <somersames@gmail.com>
2018-05-23 14:53:18 +0800

    测试注册中心，Zuul和用户的微服务

---
+++ scuser/src/main/java/com/somersames/UserConfigApplication.java
@@ -0,0 +1,17 @@
+package com.somersames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author szh
+ * @create 2018-05-23 14:33
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class UserConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserConfigApplication.class);
+    }
+}
--- scuser/src/main/java/com/somersames/controller/UserTestController.java
+++ scuser/src/main/java/com/somersames/controller/UserTestController.java
@@ -1,10 +1,21 @@
 package com.somersames.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 测试类的Controller
  *
  * @author szh
  * @create 2018-05-22 10:29
  **/
+@RestController
+@RequestMapping("user")
 public class UserTestController {
+    @GetMapping("/testzuul")
+    public String testZUul(){
+        return "测试Zuul";
+    }
 }
